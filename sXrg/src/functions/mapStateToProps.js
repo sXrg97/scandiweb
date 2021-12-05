26a5201e9069380cd40fd4c536f4d5b6b607f753
Arnold <arnoldmitrica@gmail.com>
2021-12-05 19:50:16 +0200

    yolo

--- sXrg/src/functions/mapStateToProps.js
+++ sXrg/src/functions/mapStateToProps.js
@@ -1,5 +1,6 @@
 export const mapStateToProps = (state) => ({
-  products: state.products,
-  product: state.product,
-  currency: state.currency,
+	products: state.products,
+	product: state.product,
+	currency: state.currency,
+	cart: state.cart,
 });
--- sXrg/src/reducers/rootReducer.js
+++ sXrg/src/reducers/rootReducer.js
@@ -1,12 +1,14 @@
 import products from "./products";
 import product from "./product";
 import currency from "./currency";
+import cart from "./cart";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
-  products,
-  product,
-  currency,
+	products,
+	product,
+	currency,
+	cart,
 });
 
 export default rootReducer;
