6a3122dfb7fc4487b3fe29373af099755bd2b0dd
Arnold <arnoldmitrica@gmail.com>
2021-12-09 15:30:43 +0200

    Add products to cart

--- sXrg/src/reducers/cart.js
+++ sXrg/src/reducers/cart.js
@@ -1,14 +1,56 @@
 import { addToCart, removeFromCart, clearCart } from "./actionNames";
 
-const initialState = { products: [] };
+const initialState = {
+	list: { variants: [] },
+};
 
 const cart = (state = initialState, action) => {
 	switch (action.type) {
 		case addToCart:
-			return {
-				...state,
-				products: [...state.products, action.payload.name],
-			};
+			let found = false;
+			//Check if the product to be added to the cart is already existing in the cart with the same attributes
+			state.list.variants.map((variant) => {
+				console.log(
+					"DEBUG",
+					variant.name,
+					action.payload.name,
+					variant.attributes,
+					action.payload.attributes,
+					JSON.stringify(variant.attributes) ===
+						JSON.stringify(action.payload.attributes)
+				);
+				if (
+					variant.name === action.payload.name &&
+					variant.attributes === action.payload.attributes
+				) {
+					variant.amount++; //if found increase the amount
+					found = true;
+				}
+			});
+			//
+			//As we already increased the amount we just return the state
+			if (found) {
+				return {
+					list: {
+						variants: [...state.list.variants],
+					},
+				};
+			}
+			//If we didn't find the product, return the state and add the product to the state
+			else {
+				return {
+					list: {
+						variants: [
+							...state.list.variants,
+							{
+								name: action.payload.name,
+								attributes: action.payload.attributes,
+								amount: 1,
+							},
+						],
+					},
+				};
+			}
 		// case removeFromCart:
 		//   return {
 		//     ...state,
