e721bb51e811374d2ecf535bd0481c4cfb4d1bf7
sXrg97 <sergiumarcu97@gmail.com>
2021-12-09 23:50:58 +0200

    Fixed object comparing(now using Lodash). Added ability to remove items and delete the object variant if the amount goes to 0.

--- sXrg/src/reducers/cart.js
+++ sXrg/src/reducers/cart.js
@@ -1,71 +1,81 @@
 import { addToCart, removeFromCart, clearCart } from "./actionNames";
+import { isEqual } from "lodash";
 
-const initialState = {
-	list: { variants: [] },
-};
-
-const cart = (state = initialState, action) => {
-	switch (action.type) {
-		case addToCart:
-			let found = false;
-			//Check if the product to be added to the cart is already existing in the cart with the same attributes
-			state.list.variants.map((variant) => {
-				console.log(
-					"DEBUG",
-					variant.name,
-					action.payload.name,
-					variant.attributes,
-					action.payload.attributes,
-					JSON.stringify(variant.attributes) ===
-						JSON.stringify(action.payload.attributes)
-				);
-				if (
-					variant.name === action.payload.name &&
-					variant.attributes === action.payload.attributes
-				) {
-					variant.amount++; //if found increase the amount
-					found = true;
-				}
-			});
-			//
-			//As we already increased the amount we just return the state
-			if (found) {
-				return {
-					list: {
-						variants: [...state.list.variants],
-					},
-				};
-			}
-			//If we didn't find the product, return the state and add the product to the state
-			else {
-				return {
-					list: {
-						variants: [
-							...state.list.variants,
-							{
-								name: action.payload.name,
-								attributes: action.payload.attributes,
-								amount: 1,
-							},
-						],
-					},
-				};
-			}
-		// case removeFromCart:
-		//   return {
-		//     ...state,
-		//     selectedCurrency: action.payload.currency,
-		//   };
-		// case clearCart:
-		//   return {
-		//     ...state,
-		//     selectedCurrency: action.payload.currency,
-		//   };
-		default:
-			return {
-				...state,
-			};
-	}
+const cart = (
+  state = {
+    list: { variants: [] },
+  },
+  action
+) => {
+  switch (action.type) {
+    case addToCart:
+      let found = false;
+      //Check if the product to be added to the cart is already existing in the cart with the same attributes
+      state.list.variants.map((variant) => {
+        if (variant) {
+          if (
+            isEqual(variant.name, action.payload.name) &&
+            isEqual(variant.attributes, action.payload.attributes)
+          ) {
+            variant.amount++; //if found increase the amount
+            found = true;
+          }
+        }
+      });
+      //
+      //As we already increased the amount we just return the state
+      if (found) {
+        return {
+          list: {
+            variants: [...state.list.variants],
+          },
+        };
+      }
+      //If we didn't find the product, return the state and add the product to the state
+      else {
+        return {
+          list: {
+            variants: [
+              ...state.list.variants,
+              {
+                name: action.payload.name,
+                attributes: action.payload.attributes,
+                amount: 1,
+              },
+            ],
+          },
+        };
+      }
+    case removeFromCart:
+      //Check if the product to be added to the cart is already existing in the cart with the same attributes
+      state.list.variants.map((variant, idx) => {
+        if (variant) {
+          if (
+            isEqual(variant.name, action.payload.name) &&
+            isEqual(variant.attributes, action.payload.attributes)
+          ) {
+            variant.amount--; //if found decrease the amount
+            if (variant.amount <= 0) {
+              console.error("delete", idx);
+              delete state.list.variants[idx];
+            }
+          }
+        }
+      });
+      return state;
+    //
+    //As we already increased the amount we just return the state
+    //If we didn't find the product, return the state and add the product to the state
+    // case clearCart:
+    //   return {
+    //     ...state,
+    //     selectedCurrency: action.payload.currency,
+    //   };
+    default:
+      return {
+        ...state,
+      };
+  }
 };
 
 export default cart;
--- sXrg/src/reducers/productActions.js
+++ sXrg/src/reducers/productActions.js
@@ -1,41 +1,49 @@
 import {
-	setProducts,
-	setProduct,
-	toggleCurrencyModal,
-	setCurrency,
-	addToCart,
+  setProducts,
+  setProduct,
+  toggleCurrencyModal,
+  setCurrency,
+  addToCart,
+  removeFromCart,
 } from "./actionNames";
 
 export const set = (data) => {
-	return {
-		type: setProducts,
-		payload: data,
-	};
+  return {
+    type: setProducts,
+    payload: data,
+  };
 };
 
 export const setProd = (data) => {
-	return {
-		type: setProduct,
-		payload: data,
-	};
+  return {
+    type: setProduct,
+    payload: data,
+  };
 };
 
 export const toggleCurrency = () => {
-	return {
-		type: toggleCurrencyModal,
-	};
+  return {
+    type: toggleCurrencyModal,
+  };
 };
 
 export const setCurr = (data) => {
-	return {
-		type: setCurrency,
-		payload: data,
-	};
+  return {
+    type: setCurrency,
+    payload: data,
+  };
 };
 
 export const addCart = (data) => {
-	return {
-		type: addToCart,
-		payload: data,
-	};
+  return {
+    type: addToCart,
+    payload: data,
+  };
+};
+
+export const removeCart = (data) => {
+  return {
+    type: removeFromCart,
+    payload: data,
+  };
 };
