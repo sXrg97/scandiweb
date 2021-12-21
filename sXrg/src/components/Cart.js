cc7c1c2cc37443f226945b7b9c362a4049a77153
sXrg97 <sergiumarcu97@gmail.com>
2021-12-22 01:51:14 +0200

    Fixes, cleanups, progress on Cart component

--- sXrg/src/components/Cart.js
+++ sXrg/src/components/Cart.js
@@ -1,13 +1,75 @@
 import React from "react";
+import { connect } from "react-redux";
+import currencySymbolGetter from "../functions/currencySymbolGetter";
+import { mapStateToProps } from "../functions/mapStateToProps";
+import { withParams } from "../functions/withParams";
+import "./Cart.css";
 
 class Cart extends React.Component {
-	render() {
-		return (
-			<>
-				<div>Cart</div>
-			</>
-		);
-	}
+  render() {
+    return (
+      <>
+        <div className="centered__container">
+          <h1 className="cart__title text text_weight_700">Cart</h1>
+          <div className="cart__items">
+            {this.props.cart.list.variants.map((variant) => (
+              <div className="cart__item">
+                <div className="cart__item__details">
+                  <div className="cart__item__details__namecontainer">
+                    <div className="cart__item__details__brand text text_weight_600">
+                      {variant.brand}
+                    </div>
+                  </div>
+                  <div className="cart__item__details__name text">
+                    {variant.name}
+                  </div>
+                  <div className="cart__item__details__price text text_weight_700">
+                    {currencySymbolGetter(this.props.currency.selectedCurrency)}
+                    {variant.prices.map((price) =>
+                      price.currency === this.props.currency.selectedCurrency
+                        ? (price.amount * variant.amount).toFixed(2)
+                        : ""
+                    )}
+                  </div>
+                  <div className="cart__item__details__attributes">
+                    {variant.attributes.map((attr) => {
+                      if (attr.attribs.type === "text") {
+                        return (
+                          <div
+                            title={attr.id}
+                            className="cart__item__details__attribute"
+                          >
+                            {attr.attribs.value}
+                          </div>
+                        );
+                      } else if (attr.attribs.type === "swatch") {
+                        return (
+                          <div
+                            title={attr.id}
+                            className="cart__item__details__attribute"
+                            style={{
+                              backgroundColor: attr.attribs.value,
+                            }}
+                          ></div>
+                        );
+                      } else {
+                        console.error(
+                          "Attribute type not found, check Cart.js component"
+                        );
+                        return <div></div>;
+                      }
+                    })}
+                  </div>
+                </div>
+                <div className="cart__item__buttons"></div>
+                <div className="cart__item__photos">a</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
-export default Cart;
+export default connect(mapStateToProps)(withParams(Cart));
--- sXrg/src/components/Category.js
+++ sXrg/src/components/Category.js
@@ -8,141 +8,139 @@ import getData from "../functions/getData";
 import { Link } from "react-router-dom";
 import { ReactComponent as CartSVG } from "../img/cart.svg";
 import currencySymbolGetter from "../functions/currencySymbolGetter";
-import { isEqual } from "lodash";
 
 class Category extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			params: this.props.params.category,
-			attributes: [],
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      params: this.props.params.category,
+      attributes: [],
+    };
+  }
 
-	handleResize() {
-		document
-			.querySelectorAll(".category__product__img img")
-			.forEach((element) => {
-				const width = element.offsetWidth;
-				const addBtn = element.parentElement.parentElement.previousSibling;
-				addBtn.style.top = width - 15 + "px";
-			});
-	}
+  handleResize() {
+    document
+      .querySelectorAll(".category__product__img img")
+      .forEach((element) => {
+        const width = element.offsetWidth;
+        const addBtn = element.parentElement.parentElement.previousSibling;
+        addBtn.style.top = width - 15 + "px";
+      });
+  }
 
-	async componentDidMount() {
-		getData(this.props.params.category).then((data) => {
-			this.props.dispatch(set({ gotData: true, data: data }));
-		});
+  async componentDidMount() {
+    getData(this.props.params.category).then((data) => {
+      this.props.dispatch(set({ gotData: true, data: data }));
+    });
 
-		// This might reduce performance
-		window.addEventListener("resize", this.handleResize);
-	}
+    // This might reduce performance
+    window.addEventListener("resize", this.handleResize);
+  }
 
-	async componentDidUpdate(prevProps) {
-		if (this.state.params !== this.props.params.category) {
-			this.setState((state, props) => {
-				return { ...state, params: props.params.category };
-			});
-		}
+  async componentDidUpdate(prevProps) {
+    if (this.state.params !== this.props.params.category) {
+      this.setState((state, props) => {
+        return { ...state, params: props.params.category };
+      });
+    }
 
-		getData(this.props.params.category).then((data) => {
-			if (this.props.params.category !== prevProps.params.category)
-				this.props.dispatch(set({ gotData: true, data: data }));
-		});
-		this.handleResize();
-	}
+    getData(this.props.params.category).then((data) => {
+      if (this.props.params.category !== prevProps.params.category)
+        this.props.dispatch(set({ gotData: true, data: data }));
+    });
+    this.handleResize();
+  }
 
-	componentWillUnmount() {
-		this.props.products &&
-			this.props.dispatch(set({ gotData: false, data: [] }));
-	}
+  componentWillUnmount() {
+    this.props.products &&
+      this.props.dispatch(set({ gotData: false, data: [] }));
+  }
 
-	render() {
-		return (
-			<div className="category">
-				<div className="category__name"> {this.props.params.category}</div>
-				<div className="products">
-					{this.props.products.gotData &&
-						this.props.products.data.data.category.products.map((product) => {
-							return (
-								<div
-									className={`category__product__card ${
-										product.inStock ? "" : "notInStock"
-									}`}
-									key={product.id}
-								>
-									{console.error(product)}
-									<div
-										className="addToCartBtn"
-										onClick={() => {
-											let thisProductDefaultAttribs = [];
-											console.log(product);
-											product.attributes.map((attr) => {
-												thisProductDefaultAttribs.push({
-													id: attr.id,
-													attribs: {
-														id: attr.items[0].id,
-														type: attr.type,
-														displayValue: attr.items[0].displayValue,
-														value: attr.items[0].value,
-													},
-												});
-											});
-											console.log(thisProductDefaultAttribs);
-											this.props.dispatch(
-												addCart({
-													name: product.name,
-													attributes: thisProductDefaultAttribs,
-													prices: product.prices,
-													images: product.gallery,
-													brand: product.brand,
-												})
-											);
-										}}
-									>
-										<CartSVG fill="#fff" />
-									</div>
-									<Link className="link" to={`/product/${product.id}`}>
-										<div className="category__product__img">
-											<img src={product.gallery[0]} alt={product.name} />
-											{product.inStock ? (
-												""
-											) : (
-												<div className="notInStockOverlay">Out of stock</div>
-											)}
-										</div>
-										<div className="category__product__content">
-											<div
-												className={`category__product__name ${
-													product.inStock ? "" : "notInStock"
-												}`}
-											>
-												{product.brand} {product.name}
-											</div>
-											<div
-												className={`category__product__price ${
-													product.inStock ? "" : "notInStock"
-												}`}
-											>
-												{currencySymbolGetter(
-													this.props.currency.selectedCurrency
-												)}
-												{product.prices.map((price) =>
-													price.currency ===
-													this.props.currency.selectedCurrency
-														? price.amount
-														: ""
-												)}
-											</div>
-										</div>
-									</Link>
-								</div>
-							);
-						})}
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="category">
+        <div className="category__name"> {this.props.params.category}</div>
+        <div className="products">
+          {this.props.products.gotData &&
+            this.props.products.data.data.category.products.map((product) => {
+              return (
+                <div
+                  className={`category__product__card ${
+                    product.inStock ? "" : "notInStock"
+                  }`}
+                  key={product.id}
+                >
+                  {console.error(product)}
+                  <div
+                    className="addToCartBtn"
+                    onClick={() => {
+                      let thisProductDefaultAttribs = [];
+                      console.log(product);
+                      product.attributes.map((attr) => {
+                        thisProductDefaultAttribs.push({
+                          id: attr.id,
+                          attribs: {
+                            id: attr.items[0].id,
+                            type: attr.type,
+                            displayValue: attr.items[0].displayValue,
+                            value: attr.items[0].value,
+                          },
+                        });
+                      });
+                      this.props.dispatch(
+                        addCart({
+                          name: product.name,
+                          attributes: thisProductDefaultAttribs,
+                          prices: product.prices,
+                          images: product.gallery,
+                          brand: product.brand,
+                        })
+                      );
+                    }}
+                  >
+                    <CartSVG fill="#fff" />
+                  </div>
+                  <Link className="link" to={`/product/${product.id}`}>
+                    <div className="category__product__img">
+                      <img src={product.gallery[0]} alt={product.name} />
+                      {product.inStock ? (
+                        ""
+                      ) : (
+                        <div className="notInStockOverlay">Out of stock</div>
+                      )}
+                    </div>
+                    <div className="category__product__content">
+                      <div
+                        className={`category__product__name ${
+                          product.inStock ? "" : "notInStock"
+                        }`}
+                      >
+                        {product.brand} {product.name}
+                      </div>
+                      <div
+                        className={`category__product__price ${
+                          product.inStock ? "" : "notInStock"
+                        }`}
+                      >
+                        {currencySymbolGetter(
+                          this.props.currency.selectedCurrency
+                        )}
+                        {product.prices.map((price) =>
+                          price.currency ===
+                          this.props.currency.selectedCurrency
+                            ? price.amount
+                            : ""
+                        )}
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default connect(mapStateToProps)(withParams(Category));
