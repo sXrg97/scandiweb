7917322686ed08db1d208f427c65b141f34ff343
sXrg97 <sergiumarcu97@gmail.com>
2022-01-07 03:01:17 +0200

    Changed favicon & title

--- sXrg/src/App.js
+++ sXrg/src/App.js
@@ -8,23 +8,23 @@ import Checkout from "./components/Checkout";
 import "./App.css";
 
 class App extends React.Component {
-	render() {
-		return (
-			<BrowserRouter>
-				<div className="app">
-					<Header />
-					<Routes>
-						<Route path="/" element={<Navigate replace to="/category/all" />} />
-						<Route path="/category" element={<Category />} />
-						<Route path="/category/:category" element={<Category />} />
-						<Route path="/product/:productId" element={<Product />} />
-						<Route path="/cart" element={<Cart />} />
-						<Route path="/checkout" element={<Checkout />} />
-					</Routes>
-				</div>
-			</BrowserRouter>
-		);
-	}
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="app">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Navigate replace to="/category/all" />} />
+            <Route path="/category" element={<Category />} />
+            <Route path="/category/:category" element={<Category />} />
+            <Route path="/product/:productId" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
